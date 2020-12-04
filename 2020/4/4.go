b3261b8203873b7af9fbf3566f3f6f7a3f7d9627
Willie Abrams <willie@pobox.com>
2020-12-04 09:51:47 -0600

    day 4, part 2 complete

--- 2020/4/4.go
+++ 2020/4/4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"strings"
 
 	"github.com/willie/advent/aoc"
@@ -17,8 +18,10 @@ var passportFields = map[string]bool{
 	"hcl": false,
 	"ecl": false,
 	"pid": false,
-	"cid": true,
+	// "cid": true,
 }
+
+var eyeColor = aoc.NewStringSet("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
 func parse(in string) (passports []map[string]string) {
 	lines := strings.Split(in, "\n\n")
@@ -35,10 +38,12 @@ func parse(in string) (passports []map[string]string) {
 			}
 
 			kv := strings.Split(f, ":")
-
 			name := kv[0]
-			value := kv[1]
+			if _, there := passportFields[name]; !there {
+				continue
+			}
 
+			value := kv[1]
 			passport[name] = value
 		}
 
@@ -48,26 +53,78 @@ func parse(in string) (passports []map[string]string) {
 }
 
 func part1(in string) (valid int) {
-	passports := parse(in)
-
-	for _, passport := range passports {
-		pass := false
-
-		if len(passport) == len(passportFields) {
-			pass = true
-		}
-
-		if len(passport)+1 == len(passportFields) {
-			if _, there := passport["cid"]; !there {
-				pass = true
-			}
-		}
-
+	for _, passport := range parse(in) {
+		pass := len(passport) == len(passportFields)
 		if pass {
 			valid++
 		}
+		// fmt.Println(passport, pass)
+	}
+	return
+}
 
-		fmt.Println(passport, pass)
+func part2(in string) (count int) {
+	for _, passport := range parse(in) {
+		passportValid := true
+
+		for k, v := range passport {
+			valid := false
+
+			switch k {
+			case "byr":
+				year := aoc.AtoI(v)
+				valid = 1920 <= year && year <= 2002
+
+			case "iyr":
+				year := aoc.AtoI(v)
+				valid = 2010 <= year && year <= 2020
+
+			case "eyr":
+				year := aoc.AtoI(v)
+				valid = 2020 <= year && year <= 2030
+
+			case "hgt":
+				var h int
+				var unit string
+				fmt.Sscanf(v, "%d%s", &h, &unit)
+				switch unit {
+				case "cm":
+					valid = 150 <= h && h <= 193
+				case "in":
+					valid = 59 <= h && h <= 76
+				}
+
+			case "hcl":
+				var c color.RGBA
+				n, _ := fmt.Sscanf(v, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+				valid = n == 3
+
+			case "ecl":
+				valid = eyeColor.Contains(v)
+
+			case "pid":
+				var pid int
+				n, _ := fmt.Sscanf(v, "%9d", &pid)
+				valid = n == 1 && len(v) == 9
+
+			default:
+				fmt.Println(k, v, "not validated")
+				continue
+			}
+
+			if valid {
+				// fmt.Println(k, "valid:\t", v)
+			} else {
+				passportValid = false
+				// fmt.Println(k, "invalid:\t", v)
+			}
+		}
+
+		if passportValid && (len(passport) == len(passportFields)) {
+			count++
+		}
+
+		// fmt.Println()
 	}
 
 	return
@@ -82,8 +139,9 @@ func main() {
 	aoc.Test("test1", part1(aoc.String("test")), 2)
 	aoc.Run("part1", part1(aoc.String(day)))
 
-	// println("-------")
+	println("-------")
 
-	// aoc.Test("test2", part2(aoc.Strings("test")), 336)
-	// aoc.Run("part2", part2(aoc.Strings(day)))
+	aoc.Test("test2", part2(aoc.String("invalid")), 0)
+	aoc.Test("test2", part2(aoc.String("valid")), 4)
+	aoc.Run("part2", part2(aoc.String(day)))
 }
