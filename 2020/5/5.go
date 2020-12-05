2e24cdf3269393a07709f70e8bbe00e67bf2af1d
Willie Abrams <willie@pobox.com>
2020-12-05 10:47:23 -0600

    binary realization

--- 2020/5/5.go
+++ 2020/5/5.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/willie/advent/aoc"
 )
@@ -33,7 +35,7 @@ func calcPass(pass string) (r, c, seatID int) {
 	}
 
 	seatID = rl*8 + cl
-	fmt.Println(rl, rh, cl, ch, seatID)
+	// fmt.Println(rl, rh, cl, ch, seatID)
 	return
 }
 
@@ -57,7 +59,7 @@ func part2(in []string) (seatID int) {
 	}
 
 	sort.Ints(seatIDs)
-	fmt.Println(seatIDs)
+	// fmt.Println(seatIDs)
 
 	assigned := aoc.NewIntSet(seatIDs...)
 	for i := seatIDs[1]; i < seatIDs[len(seatIDs)-2]; i++ {
@@ -77,4 +79,32 @@ func main() {
 	aoc.Test("test1", part1(aoc.Strings("test")), 820)
 	aoc.Run("part1", part1(aoc.Strings(day)))
 	aoc.Run("part2", part2(aoc.Strings(day)))
+
+	println("-------")
+
+	fmt.Println("binary solution after some sleep, damnit")
+
+	seatIDs := []int{}
+	bc := strings.NewReplacer("F", "0", "B", "1", "L", "0", "R", "1")
+
+	// for _, pass := range aoc.Strings("test") {
+	for _, pass := range aoc.Strings(day) {
+		pass = bc.Replace(pass) // convert to binary
+		seatID, _ := strconv.ParseUint(pass, 2, 10)
+		seatIDs = append(seatIDs, int(seatID))
+
+		// r, _ := strconv.ParseUint(pass[:7], 2, 7)
+		// c, _ := strconv.ParseUint(pass[7:], 2, 3)
+		// fmt.Println(r, c, seatID)
+	}
+
+	sort.Ints(seatIDs)
+	fmt.Println("part1", seatIDs[len(seatIDs)-1])
+
+	for i, id := range seatIDs {
+		if id+1 != seatIDs[i+1] {
+			fmt.Println("part2", id+1)
+			return
+		}
+	}
 }
