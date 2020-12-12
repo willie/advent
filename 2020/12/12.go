feec091ad222d6d58cc585c50c801cdf517552e2
Willie Abrams <willie@pobox.com>
2020-12-12 11:13:39 -0600

    day 12 part 2
    not clever, but it works

--- 2020/12/12.go
+++ 2020/12/12.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"log"
 
 	"github.com/willie/advent/aoc"
 )
+
+func delta(direction string, amount int) (d image.Point) {
+	switch direction {
+	case "N":
+		d = image.Pt(0, amount)
+	case "S":
+		d = image.Pt(0, -amount)
+	case "E":
+		d = image.Pt(amount, 0)
+	case "W":
+		d = image.Pt(-amount, 0)
+	}
+	return
+}
+
+func rotate(i image.Point, direction string, amount int) (n image.Point) {
+	for n = i; amount != 0; amount = amount - 90 {
+		switch direction {
+		case "L":
+			n.X, n.Y = -n.Y, n.X
+		case "R":
+			n.X, n.Y = n.Y, -n.X
+		}
+	}
+	return
+}
 
 func part1(in []string) (first int) {
 	loc := image.Pt(0, 0)
@@ -23,32 +48,40 @@ func part1(in []string) (first int) {
 			instruction = facing[direction]
 		}
 
-		var delta image.Point
 		switch instruction {
-		case "N":
-			delta = image.Pt(0, amount)
-		case "S":
-			delta = image.Pt(0, -amount)
-		case "E":
-			delta = image.Pt(amount, 0)
-		case "W":
-			delta = image.Pt(-amount, 0)
+		case "N", "S", "E", "W":
+			loc = loc.Add(delta(instruction, amount))
 		case "L":
 			direction = (direction + step) % 4
-
-			// direction = (amount + direction) % 360
 		case "R":
 			direction = (direction + (3 * step)) % 4
-			// direction = (amount + direction + 180) % 360
 		default:
 			log.Fatalln("unknown instruction", instruction)
 		}
-
-		fmt.Println(i, instruction, facing[direction], loc, loc.Add(delta))
-		loc = loc.Add(delta)
 	}
 
 	return aoc.ManhattanDistance(0, 0, loc.X, loc.Y)
+}
+
+func part2(in []string) (first int) {
+	ship := image.Pt(0, 0)
+	waypoint := image.Pt(10, 1)
+
+	for _, i := range in {
+		instruction := i[:1]
+		amount := aoc.AtoI(i[1:])
+
+		switch instruction {
+		case "N", "S", "E", "W":
+			waypoint = waypoint.Add(delta(instruction, amount))
+		case "L", "R":
+			waypoint = rotate(waypoint, instruction, amount)
+		case "F":
+			ship = ship.Add(waypoint.Mul(amount))
+		}
+	}
+
+	return aoc.ManhattanDistance(0, 0, ship.X, ship.Y)
 }
 
 const day = "https://adventofcode.com/2020/day/12"
@@ -60,24 +93,9 @@ func main() {
 	println("------- part1")
 
 	aoc.Test("test", part1(aoc.Strings("test")), 25)
-	fmt.Println()
 	aoc.Run("run", part1(aoc.Strings(day)))
 
-	// println("------- part2")
-
-	// g := aoc.LoadGrid("count")
-	// x, y := g.FindFirst(empty)
-	// aoc.Test("count", countVisibleOccupied(g, x, y), 8)
-
-	// g = aoc.LoadGrid("count2")
-	// x, y = g.FindFirst(empty)
-	// aoc.Test("count2", countVisibleOccupied(g, x, y), 0)
-
-	// g = aoc.LoadGrid("count3")
-	// x, y = g.FindFirst(empty)
-	// aoc.Test("count3", countVisibleOccupied(g, x, y), 0)
-
-	// aoc.Test("test", part2(aoc.LoadGrid("test")), 26)
-
-	// aoc.Run("run", part2(aoc.LoadGrid(day)))
+	println("------- part2")
+	aoc.Test("test", part2(aoc.Strings("test")), 286)
+	aoc.Run("run", part2(aoc.Strings(day)))
 }
