4c2d510eb14a72d990bbbe3a40e64671f506e22f
Willie Abrams <willie@pobox.com>
2020-12-12 22:48:55 -0600

    my fingers are ready

---
+++ 2020/13/13.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/willie/advent/aoc"
+)
+
+func part1(in []string) (first int) {
+	return
+}
+
+func part2(in []string) (first int) {
+	return
+}
+
+const day = "https://adventofcode.com/2020/day/13"
+
+func main() {
+	println(day)
+	aoc.Input(day)
+
+	println("------- part1")
+
+	aoc.Test("test", part1(aoc.Strings("test")), 25)
+	// aoc.Run("run", part1(aoc.Strings(day)))
+
+	// println("------- part2")
+	// aoc.Test("test", part2(aoc.Strings("test")), 286)
+	// aoc.Run("run", part2(aoc.Strings(day)))
+}
