25b32fa835508913654c3c576a92b0b91a8510a8
Willie Abrams <willie@pobox.com>
2020-12-15 08:54:53 -0600

    that was brutal

--- 2020/15/15.go
+++ 2020/15/15.go
@@ -23,15 +23,11 @@ func part1(in string, turns int) (result [2]int) {
 		this := 0
 
 		all := spoken.AllIndex(last)
-		// fmt.Println(turn, last, spoken, all)
 		switch {
 		case len(all) <= 1: // never been spoken
 			this = 0
 
 		default:
-			// fmt.Println(turn, last, spoken)
-			// fmt.Println(all)
-
 			this = all[len(all)-1] - all[len(all)-2]
 		}
 
@@ -47,13 +43,44 @@ func part1(in string, turns int) (result [2]int) {
 	return
 }
 
+func part2(in string, turns int) (result [2]int) {
+	spoken := map[int]int{}
+	var last int
+
+	for i, x := range strings.Split(in, ",") {
+		last := aoc.AtoI(x)
+		spoken[last] = i + 1
+	}
+
+	for turn := len(spoken) + 1; turn < turns; turn++ {
+		if prev, ok := spoken[last]; ok {
+			spoken[last] = turn
+			last = turn - prev
+		} else {
+			spoken[last] = turn
+			last = 0
+		}
+	}
+
+	result[0] = last
+
+	return
+}
+
 const day = "https://adventofcode.com/2020/day/15"
 
 func main() {
 	println(day)
 	aoc.Input(day)
 
-	println("------- combined")
+	println("------- part 1")
 	fmt.Println("test", part1(aoc.String("test"), 2020), 436)
-	fmt.Println("run", part1(aoc.String(day), 2020))
+	fmt.Println("test", part1(aoc.String("test"), 9), 4)
+	// fmt.Println("run", part1(aoc.String(day), 2020))
+
+	println("------- part 2")
+	fmt.Println("test", part2(aoc.String("test"), 2020), 436)
+	fmt.Println("test2", part2(aoc.String("test"), 9), 4)
+	fmt.Println("run", part2(aoc.String(day), 2020))
+	fmt.Println("run", part2(aoc.String(day), 30000000))
 }
