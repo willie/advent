7023bc31033b8b2d3898c3b20aed9fba81d9e6b6
Willie Abrams <willie@pobox.com>
2020-12-16 12:35:51 -0600

    not my best work, not the worst

--- 2020/16/16.go
+++ 2020/16/16.go
@@ -7,10 +7,8 @@ import (
 	"github.com/willie/advent/aoc"
 )
 
-// var validation = map[int][]string{}
-
 func parseTickets(in string) (tickets []aoc.Ints) {
-	for _, tix := range strings.Split(in, "\n") {
+	for _, tix := range strings.Split(in, "\n")[1:] {
 		ticket := aoc.Ints{}
 		for _, v := range strings.Split(tix, ",") {
 			if strings.Contains(v, ":") || (v == "") {
@@ -20,23 +18,22 @@ func parseTickets(in string) (tickets []aoc.Ints) {
 			value := aoc.AtoI(v)
 			ticket = append(ticket, value)
 		}
-
 		tickets = append(tickets, ticket)
 	}
-
 	return
 }
 
 func part1(in string) (result [2]int) {
 	parts := strings.Split(in, "\n\n")
 
+	fieldNames := aoc.StringSet{}
 	validation := map[int][]string{}
 	// parse fields, ranges
 	for _, p := range strings.Split(parts[0], "\n") {
 		row := strings.Split(p, ": ")
 
-		field := row[0]
-		ranges := aoc.IntSet{}
+		field, ranges := row[0], aoc.IntSet{}
+		fieldNames.Add(field)
 
 		for _, valid := range strings.Split(row[1], " or ") {
 			r := strings.Split(valid, "-")
@@ -47,31 +44,120 @@ func part1(in string) (result [2]int) {
 			}
 		}
 
-		// fmt.Println(validation)
 	}
+	// fmt.Println(validation)
 
 	var first int
 
 	nearbyTickets := parseTickets(parts[2])
+	var validTickets []aoc.Ints
 
 	// check invalid fields
 	for _, ticket := range nearbyTickets {
+		valid := true
+
 		for _, value := range ticket {
 			// check for invalid values
 			if _, ok := validation[value]; !ok {
 				first += value
+				valid = false
 			}
+		}
+
+		if valid {
+			validTickets = append(validTickets, ticket)
 		}
 	}
 
 	result[0] = first
 
 	// part 2
-	myTicket := parseTickets(parts[1])[1]
-	nearbyTickets = append(nearbyTickets, myTicket)
-	// for _, ticket := range nearbyTickets {
+	myTicket := parseTickets(parts[1])[0]
+	fmt.Println("my", myTicket)
 
+	// field number to field names to occurence count
+	fieldSort := []map[string]int{}
+	for range validTickets[0] {
+		fieldSort = append(fieldSort, map[string]int{})
+	}
+
+	// get the counts
+	allTickets := append(validTickets, myTicket)
+
+	for _, ticket := range allTickets {
+		for field, value := range ticket {
+			for _, name := range validation[value] {
+				fieldSort[field][name]++
+			}
+		}
+	}
+
+	fieldNameSorter := map[int]aoc.StringSet{}
+	for i := range validTickets[0] {
+		fieldNameSorter[i] = aoc.StringSet{}
+	}
+
+	for i, nameCount := range fieldSort {
+		for name, count := range nameCount {
+			if count == len(allTickets) {
+				fieldNameSorter[i].Add(name)
+			}
+		}
+	}
+	fmt.Println("fieldNameSorter", fieldNameSorter, len(allTickets))
+
+	fieldNameOrder := map[int]string{}
+	namesFound := aoc.StringSet{}
+	for len(fieldNameOrder) < len(fieldNameSorter) {
+		for n, names := range fieldNameSorter {
+			if len(names) == 1 {
+				name := names.Values()[0]
+
+				fieldNameOrder[n] = name
+
+				namesFound.Add(name)
+				// remove from others
+				for _, names := range fieldNameSorter {
+					names.Remove(name)
+				}
+			}
+		}
+	}
+
+	fmt.Println("fieldSort", fieldSort)
+
+	// for i, nameCount := range fieldSort {
+	// 	var highName string
+	// 	var highCount int
+
+	// 	for name, count := range nameCount {
+	// 		if !fieldNames.Contains(name) { // already removed
+	// 			continue
+	// 		}
+
+	// 		if count > highCount {
+	// 			highCount = count
+	// 			highName = name
+	// 		}
+	// 	}
+
+	// 	fieldNameOrder[i] = highName
+	// 	fieldNames.Remove(highName)
 	// }
+
+	for _, name := range fieldNameOrder {
+		fmt.Print(name, " ")
+	}
+	fmt.Println()
+
+	second := 1
+	for i, name := range fieldNameOrder {
+		if strings.Contains(name, "departure") {
+			second *= myTicket[i]
+		}
+	}
+
+	result[1] = second
 
 	return
 }
@@ -82,13 +168,7 @@ func main() {
 	println(day)
 	aoc.Input(day)
 
-	println("------- part 1")
-	fmt.Println("test", part1(aoc.String("test")), 71, 13)
+	fmt.Println("test", part1(aoc.String("test")), 71)
+	fmt.Println("test2", part1(aoc.String("test2")), 13)
 	fmt.Println("run", part1(aoc.String(day)))
-
-	println("------- part 2")
-	// fmt.Println("test", part2(aoc.String("test"), 2020), 436)
-	// fmt.Println("test2", part2(aoc.String("test"), 9), 4)
-	// fmt.Println("run", part2(aoc.String(day), 2020))
-	// fmt.Println("run", part2(aoc.String(day), 30000000))
 }
