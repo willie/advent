339b8cef8696532584ae5cb0952ab154ab43e81c
Willie Abrams <willie@pobox.com>
2020-12-18 07:40:40 -0600

    for later

---
+++ aoc/stack.go
@@ -0,0 +1,22 @@
+// type stack struct {
+// 	s []int
+// }
+
+// func (s *stack) push(i int) {
+// 	s.s = append(s.s, i)
+// }
+
+// func (s *stack) pop() (top int) {
+// 	top = s.top()
+// 	s.s = s.s[:len(s.s)-1]
+// 	return
+// }
+
+// func (s *stack) top() (top int) {
+// 	top = s.s[len(s.s)-1]
+// 	return
+// }
+
+// func (s *stack) size() int {
+// 	return len(s.s)
+// }
