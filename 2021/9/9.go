72c9941eec65ddcdacb2ea4f5cd0137a224d004e
Willie Abrams <willie@pobox.com>
2021-12-08 23:46:14 -0600

    tweaks for fill

--- 2021/9/9.go
+++ 2021/9/9.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"image"
 
 	"github.com/willie/advent/aoc"
 )
@@ -18,20 +18,10 @@ func part1(in []string) (result int) {
 		next = true
 		values := []int{}
 
-		if i := g.Get(x-1, y, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
-		}
-
-		if i := g.Get(x+1, y, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
-		}
-
-		if i := g.Get(x, y+1, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
-		}
-
-		if i := g.Get(x, y-1, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
+		for _, diff := range adjacent {
+			if i := g.Get(x+diff.X, y+diff.Y, invalid); i != invalid {
+				values = append(values, aoc.AtoI(i))
+			}
 		}
 
 		if len(values) == 0 {
@@ -41,8 +31,7 @@ func part1(in []string) (result int) {
 		c := aoc.AtoI(s)
 		if c < aoc.Min(values...) {
 			result += (1 + c)
-
-			fmt.Println(x, y, " -> ", c)
+			// fmt.Println(x, y, " -> ", c)
 		}
 
 		return
@@ -51,26 +40,24 @@ func part1(in []string) (result int) {
 	return
 }
 
+var (
+	adjacent = []image.Point{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
+)
+
 func part2(in []string) (result int) {
-	// lowpoints := []int{}
+	// points := map[image.Point]int{}
 
 	g := aoc.NewGrid(in)
 	g.Iterate(func(x, y int, s string) (next bool) {
 		next = true
 		values := []int{}
 
-		if i := g.Get(x-1, y, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
+		for _, diff := range adjacent {
+			if i := g.Get(x+diff.X, y+diff.Y, invalid); i != invalid {
+				values = append(values, aoc.AtoI(i))
+			}
 		}
-		if i := g.Get(x+1, y, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
-		}
-		if i := g.Get(x, y+1, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
-		}
-		if i := g.Get(x, y-1, invalid); i != invalid {
-			values = append(values, aoc.AtoI(i))
-		}
+
 		if len(values) == 0 {
 			return
 		}
@@ -78,8 +65,7 @@ func part2(in []string) (result int) {
 		c := aoc.AtoI(s)
 		if c < aoc.Min(values...) {
 			result += (1 + c)
-
-			fmt.Println(x, y, " -> ", c)
+			// fmt.Println(x, y, " -> ", c)
 		}
 
 		return
@@ -94,10 +80,10 @@ func main() {
 	println(day)
 
 	aoc.Test("test1", part1(aoc.Strings("test")), 15)
-	// aoc.Test("test2", part2(aoc.Strings("test")), 12)
+	aoc.Test("test2", part2(aoc.Strings("test")), 15)
 
 	println("-------")
 
 	aoc.Run("part1", part1(aoc.Strings(day)))
-	// aoc.Run("part2", part2(aoc.Strings(day)))
+	aoc.Run("part2", part2(aoc.Strings(day)))
 }
