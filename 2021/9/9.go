0d23a1c5832003c355a41d8e4d8ed35b80577298
Willie Abrams <willie@pobox.com>
2021-12-09 00:26:48 -0600

    fill boom

--- 2021/9/9.go
+++ 2021/9/9.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image"
+	"sort"
 
 	"github.com/willie/advent/aoc"
 )
@@ -44,10 +45,29 @@ var (
 	adjacent = []image.Point{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
 )
 
-func part2(in []string) (result int) {
-	// points := map[image.Point]int{}
+func fill(g aoc.Grid, x, y int, points map[image.Point]int) {
+	if _, ok := points[image.Point{x, y}]; ok {
+		return
+	}
 
+	if i := g.Get(x, y, invalid); i == invalid {
+		return
+	}
+
+	points[image.Point{x, y}] = 1
+
+	for _, diff := range adjacent {
+		if i := g.Get(x+diff.X, y+diff.Y, invalid); i != invalid {
+			fill(g, x+diff.X, y+diff.Y, points)
+		}
+	}
+}
+
+func part2(in []string) (result int) {
 	g := aoc.NewGrid(in)
+
+	basins := []int{}
+
 	g.Iterate(func(x, y int, s string) (next bool) {
 		next = true
 		values := []int{}
@@ -64,14 +84,20 @@ func part2(in []string) (result int) {
 
 		c := aoc.AtoI(s)
 		if c < aoc.Min(values...) {
-			result += (1 + c)
-			// fmt.Println(x, y, " -> ", c)
+			// result += (1 + c)
+			points := map[image.Point]int{}
+			fill(g, x, y, points)
+
+			basins = append(basins, len(points))
 		}
 
 		return
 	})
 
-	return
+	sort.Ints(basins)
+	basins = basins[len(basins)-3:]
+
+	return aoc.Product(basins...)
 }
 
 const day = "https://adventofcode.com/2021/day/9"
@@ -80,7 +106,7 @@ func main() {
 	println(day)
 
 	aoc.Test("test1", part1(aoc.Strings("test")), 15)
-	aoc.Test("test2", part2(aoc.Strings("test")), 15)
+	aoc.Test("test2", part2(aoc.Strings("test")), 1134)
 
 	println("-------")
 
