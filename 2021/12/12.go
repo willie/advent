65abe205ccccef68626b5bd9f8c50687229ae541
Willie Abrams <willie@pobox.com>
2021-12-11 23:08:20 -0600

    in progress

---
+++ 2021/12/12.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/willie/advent/aoc"
+)
+
+func part1(in []string) (result int) {
+
+	return
+}
+
+func part2(in []string) (result int) {
+
+	return
+}
+
+const day = "https://adventofcode.com/2021/day/12"
+
+func main() {
+	println(day)
+
+	aoc.Test("test1", part1(aoc.Strings("test"), 100), 1656)
+	// aoc.Test("test2", part2(aoc.Strings("test")), 195)
+
+	// println("-------")
+
+	// aoc.Run("part1", part1(aoc.Strings(day), 100))
+	// aoc.Run("part2", part2(aoc.Strings(day)))
+}
