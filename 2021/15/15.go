ca1e537503755dc7e9868532a1d5106dc64de95e
Willie Abrams <willie@pobox.com>
2021-12-16 00:15:51 -0600

    day 15, part 2
    finally

--- 2021/15/15.go
+++ 2021/15/15.go
@@ -61,8 +61,34 @@ func part1(in []string) (result int) {
 	return int(distance)
 }
 
-func part2(in string) (result int) {
-	return
+func part2(in []string) (result int) {
+	ig := aoc.NewGrid(in)
+	width, height := ig.Width(), ig.Height()
+
+	g := aoc.NewBlankGrid(width*5, height*5, invalid)
+
+	for xs := 0; xs < 5; xs++ {
+		for ys := 0; ys < 5; ys++ {
+			ig.Iterate(func(x, y int, s string) (next bool) {
+				next = true
+				c := aoc.AtoI(s)
+
+				c += xs + ys
+				if c > 9 {
+					c -= 9
+				}
+
+				g.Set(x+(width*xs), y+(height*ys), fmt.Sprintf("%d", c))
+				return
+			})
+		}
+	}
+
+	start := Location(&g, 0, 0)
+	end := Location(&g, g.Width()-1, g.Height()-1)
+	_, distance, _ := astar.Path(start, end)
+
+	return int(distance)
 }
 
 const day = "https://adventofcode.com/2021/day/15"
@@ -71,10 +97,10 @@ func main() {
 	println(day)
 
 	aoc.Test("test1", part1(aoc.Strings("test")), 40)
-	// aoc.Test("test2", part2(aoc.String("test")), 195)
+	aoc.Test("test2", part2(aoc.Strings("test")), 315)
 
 	println("-------")
 
 	aoc.Run("part1", part1(aoc.Strings(day)))
-	// aoc.Run("part2", part2(aoc.String(day)))
+	aoc.Run("part2", part2(aoc.Strings(day)))
 }
