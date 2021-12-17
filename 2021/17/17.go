72d422c09cc9551673e387731a10c46b4d080c9d
Willie Abrams <willie@pobox.com>
2021-12-17 00:13:08 -0600

    day 17 part 1 done, part 2 not so much

---
+++ 2021/17/17.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"strings"
+
+	"github.com/willie/advent/aoc"
+)
+
+func part1(in string) (maxY int, velocityCount int) {
+	in = strings.NewReplacer("target area: x=", "", "..", " ", ", y=", " ").Replace(in)
+	var x1, x2, y1, y2 int
+	fmt.Sscanf(in, "%d %d %d %d", &x1, &x2, &y1, &y2)
+
+	target := image.Rect(x1, y1, x2, y2)
+
+	fmt.Println(target)
+
+	for i := 1; i <= aoc.Max(aoc.Abs(x1), aoc.Abs(x2)); i++ {
+		for j := aoc.Min(y1, y2); j <= aoc.Max(aoc.Abs(y1), aoc.Abs(y2)); j++ {
+
+			probe := image.Pt(0, 0)
+			velocity := image.Pt(i, j)
+
+			// iterate steps
+
+			for probe.X <= aoc.Max(aoc.Abs(x1), aoc.Abs(x2)) && probe.Y >= aoc.Min(y1, y2) {
+				// fmt.Println(probe, velocity)
+
+				probe = probe.Add(velocity)
+
+				if probe.Y > maxY {
+					maxY = probe.Y
+				}
+
+				if velocity.X > 0 {
+					velocity.X--
+				} else if velocity.X < 0 {
+					velocity.X++
+				}
+
+				velocity.Y--
+
+				// break
+				if probe.In(target) {
+					fmt.Println(i, j)
+					// hit = true
+					velocityCount++
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Println(maxY)
+
+	return
+}
+
+func part2(in string) (result int) {
+	return
+}
+
+const day = "https://adventofcode.com/2021/day/17"
+
+func main() {
+	println(day)
+
+	t1, t2 := part1("target area: x=20..30, y=-10..-5")
+	aoc.TestX("test1", t1, t2, 45, 112)
+	// aoc.Test("test2", part2("C200B40A82"), 3)
+
+	println("-------")
+
+	// t1, t2 = part1(aoc.String(day))
+	// aoc.RunX("part1", t1, t2)
+	// aoc.Run("part2", part2(aoc.String(day)))
+}
