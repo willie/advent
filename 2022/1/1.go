ca06d7ca8d99806901edca92520428c58e31022f
Willie Abrams <willie@pobox.com>
2022-11-30 23:23:28 -0600

    good one.
    6758/6606

---
+++ 2022/1/1.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"sort"
+
+	"github.com/willie/advent/aoc"
+)
+
+func part1(in []string) int {
+	elves := make(map[int]int)
+	i := 1
+
+	for _, s := range in {
+		if s == "" {
+			i++
+			continue
+		}
+		elves[i] = elves[i] + aoc.AtoI(s)
+	}
+
+	// elf := 0
+	most := 0
+	// for e, c := range elves {
+	for _, c := range elves {
+		if c > most {
+			most = c
+			// elf = e
+		}
+	}
+
+	return most
+}
+
+func part2(in []string) int {
+	elves := make(map[int]int)
+	i := 1
+
+	for _, s := range in {
+		if s == "" {
+			i++
+			continue
+		}
+		elves[i] = elves[i] + aoc.AtoI(s)
+	}
+
+	calories := []int{}
+	// elf := 0
+	// most := 0
+	// for e, c := range elves {
+	for _, c := range elves {
+		calories = append(calories, c)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(calories)))
+
+	return calories[0] + calories[1] + calories[2]
+}
+
+const day = "https://adventofcode.com/2022/day/1"
+
+func main() {
+	println(day)
+
+	aoc.Test("test1", part1(aoc.Strings("test")), 24000)
+	aoc.Test("test2", part2(aoc.Strings("test")), 45000)
+
+	println("-------")
+
+	aoc.Run("part1", part1(aoc.Strings(day)))
+	aoc.Run("part2", part2(aoc.Strings(day)))
+}
