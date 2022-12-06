39072dc94fb7994a806e7ee2a561254f4cefc32e
Willie Abrams <willie@pobox.com>
2022-12-05 23:31:33 -0600

    YAY

--- 2022/6/main.go
+++ 2022/6/main.go
@@ -2,52 +2,43 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/willie/advent/aoc"
 )
 
-func part1(name string) {
-	stacks := map[int]*aoc.Stack[string]{}
-
-	for _, s := range aoc.Strings(name) {
-		if strings.Contains(s, "[") {
-			c := 1
-			for i := 1; i < len(s); i += 4 {
-				if stacks[c] == nil {
-					stacks[c] = &aoc.Stack[string]{}
-				}
-
-				value := s[i : i+1]
-				if value != " " {
-					stacks[c].PushBottom(value)
-				}
-				c++
-			}
-		} else if strings.Contains(s, "move") {
-			var count, from, to int
-			fmt.Sscanf(s, "move %d from %d to %d", &count, &from, &to)
-
-			for i := 0; i < count; i++ {
-				x := stacks[from].Pop()
-				stacks[to].Push(x)
-			}
+func firstMarker(s string, uniqueN int) int {
+	for i := range s {
+		marker := aoc.NewSet[rune]()
+		for _, c := range s[i : i+uniqueN] {
+			marker.Add(c)
 		}
 
+		if len(marker.Values()) == uniqueN {
+			return i + uniqueN
+		}
 	}
 
-	for i := 1; i <= len(stacks); i++ {
-		fmt.Print(stacks[i].Top())
+	return 0
+}
+
+func part1(name string) {
+	for _, s := range aoc.Strings(name) {
+		fmt.Println(firstMarker(s, 4))
 	}
-	fmt.Println()
+}
+
+func part2(name string) {
+	for _, s := range aoc.Strings(name) {
+		fmt.Println(firstMarker(s, 14))
+	}
 }
 
 func main() {
 	part1("test.txt")
-	// part1("input.txt")
+	part1("input.txt")
 
-	// fmt.Println("------")
+	fmt.Println("------")
 
-	// part2("test.txt")
-	// part2("input.txt")
+	part2("test.txt")
+	part2("input.txt")
 }
