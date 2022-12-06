3e52afe4bf7a5d0f3e3b16ca4c69ec878c11f98a
Willie Abrams <willie@pobox.com>
2022-12-05 22:20:13 -0600

    simplify

--- 2022/4/range.go
+++ 2022/4/range.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/willie/advent/aoc"
+
 type Range struct {
 	start int
 	end   int
@@ -21,25 +23,10 @@ func (r1 Range) Equal(r2 Range) bool {
 }
 
 // a little extra
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
 func (r Range) Intersection(r2 Range) Range {
 	if !r.Overlaps(r2) {
 		return Range{0, 0}
 	}
 
-	return Range{max(r.start, r2.start), min(r.end, r2.end)}
+	return Range{aoc.Max(r.start, r2.start), aoc.Min(r.end, r2.end)}
 }
