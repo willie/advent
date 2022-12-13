49caf94ee3996df91e72139635842de62d93ab30
Willie Abrams <willie@pobox.com>
2022-12-13 12:28:42 -0600

    ok, arbitrary json in golang sucks

--- 2022/13/main.go
+++ 2022/13/main.go
@@ -3,34 +3,85 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/willie/advent/aoc"
 )
 
 func part1(name string) {
+	sum := 0
+	packets := []any{}
+
 	for i, pairs := range strings.Split(aoc.String(name), "\n\n") {
 		pair := strings.Split(pairs, "\n")
 
-		var first, second any
-		if err := json.Unmarshal([]byte(pair[0]), &first); err != nil {
-			panic(err)
+		var left, right any
+		json.Unmarshal([]byte(pair[0]), &left)
+		json.Unmarshal([]byte(pair[1]), &right)
+
+		if compare(left, right) < 0 {
+			sum += i + 1
 		}
-		if err := json.Unmarshal([]byte(pair[1]), &second); err != nil {
-			panic(err)
-		}
 
-		// all = append(all, first, second)
-
-		// if equal(first, second) <= 0 {
-		// 	sum += idx + 1
-		// }
-
-		fmt.Println(i, first, second)
+		packets = append(packets, left, right)
 	}
+
+	dividers := []any{[]any{[]any{float64(2)}}, []any{[]any{float64(6)}}} // either this or more unmarshalling
+
+	packets = append(packets, dividers...)
+	sort.Slice(packets, func(i, j int) bool {
+		return compare(packets[i], packets[j]) < 0
+	})
+
+	key := 1
+	for i, packet := range packets {
+		for _, divider := range dividers {
+			if compare(packet, divider) == 0 {
+				key *= i + 1
+			}
+		}
+	}
+
+	fmt.Println(sum, key)
+}
+
+func jsonNumberToI(in any) (i int, ok bool) {
+	if f, ok := in.(float64); ok {
+		return int(f), true
+	}
+	return
+}
+
+func getOrMakeList(in any) (list []any) {
+	switch in.(type) {
+	case []any, []float64:
+		list = in.([]any)
+	case float64:
+		list = []any{in}
+	}
+	return
+}
+
+// compare returns < 0 if right, 0 if equal, > 0 if wrong
+func compare(left, right any) int {
+	if l, ok := jsonNumberToI(left); ok {
+		if r, ok := jsonNumberToI(right); ok {
+			return l - r
+		}
+	}
+
+	ll, rl := getOrMakeList(left), getOrMakeList(right)
+	for i := 0; i < len(ll) && i < len(rl); i++ {
+		if c := compare(ll[i], rl[i]); c != 0 {
+			return c
+		}
+	}
+
+	return len(ll) - len(rl)
 }
 
 func main() {
 	part1("test.txt")
-	// part1("input.txt")
+	part1("input.txt")
 }
