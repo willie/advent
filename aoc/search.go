58cf884d494d0b9fa74bf31c7ba9906809d2597e
Willie Abrams <willie@pobox.com>
2022-12-17 06:58:10 -0600

    mmm

--- aoc/search.go
+++ aoc/search.go
@@ -27,6 +27,33 @@ func BFS[T comparable](start T, goal T, neighbors func(current T) []T) []T {
 	return ret
 }
 
+// from ChatAI -- no idea if it works
+func DFS[T comparable](start T, goal T, neighbors func(current T) []T) []T {
+	S := NewStack(start)
+	visited := map[T]*T{start: nil}
+
+	for !S.Empty() {
+		current := S.Pop()
+		if current == goal {
+			break
+		}
+
+		for _, n := range neighbors(current) {
+			if _, ok := visited[n]; !ok {
+				visited[n] = &current
+				S.Push(n)
+			}
+		}
+	}
+
+	ret := []T{goal}
+	for n := visited[goal]; n != nil; n = visited[*n] {
+		ret = append(ret, *n)
+	}
+
+	return ret
+}
+
 /*
 
 func BFS[T comparable](start T, goal T, neighbors func(current T) []T) []T {
