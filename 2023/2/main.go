50d49664dc0abdb2d4e06f361826895d6acfeed0
Willie Abrams <willie@pobox.com>
2023-12-01 23:53:49 -0600

    part 2

--- 2023/2/main.go
+++ 2023/2/main.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/willie/advent/aoc"
+	"golang.org/x/exp/maps"
 )
 
 func part1(in []string) (total int) {
@@ -25,7 +26,6 @@ func part1(in []string) (total int) {
 				fmt.Sscanf(pair, "%d %s", &amount, &color)
 
 				if amount > limit[color] {
-					fmt.Println("over limit", gameID, amount, color)
 					overLimit = true
 				}
 
@@ -41,16 +41,43 @@ func part1(in []string) (total int) {
 	return aoc.Sum(games...)
 }
 
+func part2(in []string) (total int) {
+
+	for _, s := range in {
+		line := strings.Split(s, ": ")
+		var gameID int
+		fmt.Sscanf(line[0], "Game %d:", &gameID)
+
+		cubesNeeded := make(map[string]int)
+
+		for _, subs := range strings.Split(line[1], "; ") {
+			for _, pair := range strings.Split(subs, ", ") {
+				var amount int
+				var color string
+				fmt.Sscanf(pair, "%d %s", &amount, &color)
+
+				if current := cubesNeeded[color]; amount > current {
+					cubesNeeded[color] = amount
+				}
+			}
+		}
+
+		total += aoc.Product(maps.Values(cubesNeeded)...)
+	}
+
+	return
+}
+
 const day = "https://adventofcode.com/2023/day/2"
 
 func main() {
 	println(day)
 
 	aoc.Test("test1", part1(aoc.Strings("test")), 8)
-	// aoc.Test("test2", part2(aoc.Strings("test2")), 281)
+	aoc.Test("test2", part2(aoc.Strings("test2")), 2286)
 
 	println("-------")
 
 	aoc.Run("part1", part1(aoc.Strings(day)))
-	// aoc.Run("part2", part2(aoc.Strings(day)))
+	aoc.Run("part2", part2(aoc.Strings(day)))
 }
