cbab5b65482decfcb5ebe2abc7a5959cbfe486f3
Willie Abrams <willie@pobox.com>
2023-12-07 01:52:41 -0600

    I'm close, but so tired.

---
+++ 2023/7/main.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/willie/advent/aoc"
+	"golang.org/x/exp/maps"
+)
+
+const strength = "23456789TJQKA"
+
+// Every hand is exactly one type. From strongest to weakest, they are:
+
+// Five of a kind, where all five cards have the same label: AAAAA
+// Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+// Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+// Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+// Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+// One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+// High card, where all cards' labels are distinct: 23456
+
+const (
+	HighCard = iota + 1
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	FullHouse
+	FourOfAKind
+	FiveOfAKind
+)
+
+func cardinality(hand string) (cardinality map[string]int) {
+	cardinality = make(map[string]int)
+	for _, h := range hand {
+		cardinality[string(h)]++
+	}
+	return
+}
+
+func handType(hand string) (score int) {
+	cardinality := cardinality(hand)
+
+	switch len(cardinality) {
+	case 1:
+		return FiveOfAKind
+	case 2:
+		// either FourOfAKind or FullHouse
+		for _, v := range cardinality {
+			if v == 4 {
+				return FourOfAKind
+			}
+		}
+
+		return FullHouse
+	case 3:
+		// either ThreeOfAKind or TwoPair
+		for _, v := range cardinality {
+			if v == 3 {
+				return ThreeOfAKind
+			}
+		}
+		return TwoPair
+	case 4:
+		return OnePair
+	case 5:
+		return HighCard
+	}
+
+	return 0
+}
+
+type Hand struct {
+	cards string
+	bid   int
+}
+
+func (h Hand) HandType() int  { return handType(h.cards) }
+func (h Hand) HandType2() int { return handType2(h.cards) }
+
+func CompareHands(a, b Hand) int {
+	if a.HandType() < b.HandType() {
+		return -1
+	} else if a.HandType() > b.HandType() {
+		return 1
+	} else {
+		for i := 0; i < len(a.cards); i++ {
+			aa := strings.IndexByte(strength, a.cards[i])
+			bb := strings.IndexByte(strength, b.cards[i])
+
+			if aa == bb {
+				continue
+			} else if aa < bb {
+				return -1
+			} else if aa > bb {
+				return 1
+			}
+		}
+	}
+	fmt.Println("WTF", a, b)
+	return 0
+}
+
+func part1(in []string) (total int) {
+	hands := []Hand{}
+
+	for _, line := range in {
+		hand := Hand{}
+		fmt.Sscanf(line, "%s %d", &hand.cards, &hand.bid)
+		hands = append(hands, hand)
+	}
+
+	slices.SortFunc(hands, CompareHands)
+
+	for i, hand := range hands {
+		total += hand.bid * (i + 1)
+	}
+
+	return
+}
+
+func handType2(hand string) (score int) {
+	cardinality := cardinality(hand)
+
+	wild := 0
+	if w, ok := cardinality["J"]; ok {
+		wild = w
+		fmt.Println(wild, cardinality)
+		delete(cardinality, "J")
+		fmt.Println(wild, cardinality)
+	}
+
+	most := aoc.Max(maps.Values(cardinality)...)
+	for i, v := range cardinality {
+		if v == most {
+			cardinality[i] += wild
+			break
+		}
+	}
+
+	// fmt.Println(wild, cardinality)
+
+	switch len(cardinality) {
+	case 1:
+		return FiveOfAKind
+	case 2:
+		// either FourOfAKind or FullHouse
+		for _, v := range cardinality {
+			if v == 4 {
+				return FourOfAKind
+			}
+		}
+
+		return FullHouse
+	case 3:
+		// either ThreeOfAKind or TwoPair
+		for _, v := range cardinality {
+			if v == 3 {
+				return ThreeOfAKind
+			}
+		}
+		return TwoPair
+	case 4:
+		return OnePair
+	case 5:
+		return HighCard
+
+	case 0:
+		return FiveOfAKind
+	}
+
+	return 0
+}
+
+const strength2 = "J23456789TQKA"
+
+func CompareHands2(a, b Hand) int {
+	if a.HandType2() < b.HandType2() {
+		return -1
+	} else if a.HandType2() > b.HandType2() {
+		return 1
+	} else {
+		for i := 0; i < len(a.cards); i++ {
+			aa := strings.IndexByte(strength2, a.cards[i])
+			bb := strings.IndexByte(strength2, b.cards[i])
+
+			if aa == bb {
+				continue
+			} else if aa < bb {
+				return -1
+			} else if aa > bb {
+				return 1
+			}
+		}
+	}
+	fmt.Println("WTF", a, b)
+	return 0
+}
+
+func part2(in []string) (total int) {
+	hands := []Hand{}
+
+	for _, line := range in {
+		hand := Hand{}
+		fmt.Sscanf(line, "%s %d", &hand.cards, &hand.bid)
+		hands = append(hands, hand)
+	}
+
+	fmt.Println(handType2("JAAJJ"), handType2("8JJJJ"), handType2("J3JJA"))
+
+	fmt.Println(hands)
+	slices.SortFunc(hands, CompareHands2)
+	// for _, hand := range hands {
+	// 	fmt.Println(hand, handType2(hand.cards))
+	// }
+
+	fmt.Println(hands)
+	for i, hand := range hands {
+		total += hand.bid * (i + 1)
+	}
+
+	return
+}
+
+const day = "https://adventofcode.com/2023/day/7"
+
+func main() {
+	println(day)
+
+	aoc.Test("test1", part1(aoc.Strings("test")), 6440)
+	aoc.Test("test2", part2(aoc.Strings("test")), 5905)
+
+	println("-------")
+
+	aoc.Run("part1", part1(aoc.Strings(day)))
+	aoc.Run("part2", part2(aoc.Strings(day)))
+	// aoc.Run("part2", part2(aoc.Strings(day)))
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/willie/advent
 
-go 1.18
+go 1.21
 
 require (
 	github.com/beefsack/go-astar v0.0.0-20200827232313-4ecf9e304482
